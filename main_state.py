7f20001979a7c11a68579277519b69cc8d4fef5b
nkj4444 <31687469+nkj4444@users.noreply.github.com>
2017-10-24 05:20:54 +0900

    FPS
    구현

--- main_state.py
+++ main_state.py
@@ -3,51 +3,18 @@ import pico2d
 import game_framework
 import title_state
 import random
-
-
-running = True
-
-
-
-def enter():
-    global boy, grass, selectedboy, team
-    boy = Boy()
-    grass = Grass()
-
-    team = [Boy() for i in range(11)]
-    selectedboy = team[1]
-
-def exit():
-    global boy, grass
-    del(boy)
-    del(grass)
-
-class Boy:
-   def __init__(self):
-      self.x, self.y = random.randint(100, 700),90
-      self.frame = random.randint(0, 7)
-      self.image = load_image('run_animation.png')
-   def update(self):
-      self.frame = (self.frame + 1) % 8
-      self.x += 5
-      if (self.x >= 800):
-        self.x = 0
-   def draw(self):
-      self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-
-
-class Grass:
-   def __init__(self):
-      self.image = load_image('grass.png')
-   def draw(self):
-      self.image.draw(400, 30)
-
-
+import ai_boy_IQ10
+import numbers
+import json
+import get_frame_time_rate
 
 def handle_events():
     global running
     global selectedboy
     global team
+    global selectboyindex, selectindex
+
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -60,27 +27,247 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
-            elif event.key == SDLK_9:
-                selectedboy = team[9]
+
             elif event.key == SDLK_1:
-                selectedboy = team[1]
+                selectedboy = team[1-1]
+                selectindex = 0
             elif event.key == SDLK_2:
-                selectedboy = team[2]
+                selectedboy = team[2-1]
+                selectindex = 1
             elif event.key == SDLK_3:
-                selectedboy = team[3]
-            elif event.key == SDLK_3:
-                selectedboy = team[3]
+                selectedboy = team[3-1]
+                selectindex = 2
+
             elif event.key == SDLK_4:
-                selectedboy = team[4]
+                selectedboy = team[4-1]
+                selectindex = 3
             elif event.key == SDLK_5:
-                selectedboy = team[5]
+                selectedboy = team[5-1]
+                selectindex = 4
+                """
             elif event.key == SDLK_6:
                 selectedboy = team[6]
+                selectindex = 5
             elif event.key == SDLK_7:
                 selectedboy = team[7]
+                selectindex = 6
             elif event.key == SDLK_8:
                 selectedboy = team[8]
+                selectindex = 7
+            elif event.key == SDLK_9:
+                selectedboy = team[9]
+                selectindex = 8
+            elif event.key == SDLK_0:
+                selectedboy = team[0]
+                selectindex = -1
+             """
+                # json의 플레이어 수가 5이기에 나머지 키 잠금
+            elif event.key == SDLK_RIGHT:
+                selectedboy.state = 1
+                if event.key == SDLK_LEFT:
+                    selectedboy.state = 0
+            elif event.key == SDLK_LEFT:
+                selectedboy.state = 0
+                if event.key == SDLK_RIGHT:
+                    selectedboy.state = 1
 
+# 키보드 위아래 눌러서 숫자 변화시키기
+
+            elif event.key == SDLK_UP:
+                if selectindex == 4:
+                    selectindex = 0
+                else:
+                    selectindex += 1
+                selectedboy = team[selectindex]
+            elif event.key == SDLK_DOWN:
+                if selectindex == 0:
+                    selectindex = 4
+                else:
+                    selectindex -= 1
+                selectedboy = team[selectindex]
+
+
+
+
+# 키보드에서 손 떼었을때
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                selectedboy.state = 3
+                if event.key == SDLK_LEFT:
+                    selectedboy.state = 2
+
+            elif event.key == SDLK_LEFT:
+                selectedboy.state = 2
+                if event.key == SDLK_RIGHT:
+                    selectedboy.state = 3
+
+
+def main():
+    global boy, grass, selectedboy, team, running
+
+
+
+
+    open_canvas(sync=True)
+    boy = Boy()
+    grass = Grass()
+
+
+
+def create_team():
+
+
+    player_state_table = {
+        "LEFT_RUN" : Boy.LEFT_RUN,
+        "RIGHT_RUN": Boy.RIGHT_RUN,
+        "LEFT_STAND": Boy.LEFT_STAND,
+        "RIGHT_STAND": Boy.RIGHT_STAND
+
+    }
+
+
+    team_data_file = open('team_data.json', 'r')
+    team_data = json.load(team_data_file)
+    team_data_file.close()
+
+    team = []
+    for name in team_data:
+        player = Boy()
+        player.name = name
+        player.x = team_data[name]['x']
+        player.y = team_data[name]['y']
+        player.state = player_state_table[team_data[name]['StartState']]
+        team.append(player)
+    return team
+
+
+
+
+current_time = get_time()
+
+def enter():
+    global boy, grass, selectedboy, team, selectboyindex, selectindex,frame_time
+    boy = Boy()
+    grass = Grass()
+    maxboy = 10
+    selectindex = 0
+    selectboyindex = 1
+    #team = [Boy() for i in range(maxboy)]
+    #maxboy 수 만큼 보이들을 생성
+
+    team = create_team()
+
+
+
+    selectedboy = team[0]
+    running = True
+    while running:
+
+        handle_events()
+        frame_time = get_frame_time_rate.FPS()
+
+        for boy in team:
+            boy.update(frame_time)
+
+        clear_canvas()
+        grass.draw()
+        numbers.draw(selectindex + 1, 740, 540)
+
+        for boy in team:
+            boy.draw()
+        for i in range(5):
+            numbers.draw(i+1, team[i].x, team[i].y + 45, 0.3)
+
+
+# 보이들 각자에 그려질 숫자들은 0~ maxboy 까지 였으나 json을 위해 임의로 수정(maxboy->5)
+
+
+        update_canvas()
+
+        delay(0.05)
+    close_canvas()
+
+
+
+
+
+
+def exit():
+    global boy, grass
+    del(boy)
+    del(grass)
+
+
+
+if __name__ == '__main__':
+    main()
+""
+class Boy:
+
+   PIXEL_PER_METER = (10.0 / 0.3)      # 10 pixel 30 cm
+   RUN_SPEED_KMPH = 20.0
+   RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+   RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+   RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+   TIME_PER_ACTION = 0.5
+   ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+   FRAMES_PER_ACTION = 8
+   total_frames = 0
+
+   image = None
+
+
+   LEFT_RUN, RIGHT_RUN = 0, 1
+   LEFT_STAND, RIGHT_STAND = 2, 3
+
+   def __init__(self):
+      self.x, self.y = random.randint(100, 700),90
+      self.frame = random.randint(0, 7)
+      self.dir = 1
+      self.state = self.RIGHT_RUN
+      self.run_frames = 0
+      self.stand_frames = 0
+      if Boy.image == None:
+        Boy.image = load_image('animation_sheet.png')
+
+   def update(self, frame_time):
+
+       distance = Boy.RUN_SPEED_PPS * frame_time
+       self.total_frames += Boy.FRAMES_PER_ACTION * Boy.ACTION_PER_TIME * frame_time
+       self.frame = int(self.total_frames) % 8
+
+       if self.state == self.RIGHT_RUN or self.state == self.LEFT_RUN:
+           self.x += (self.dir * distance)
+
+
+       if self.state == self.RIGHT_RUN:
+           self.dir = 1
+           #self.frame = (self.frame + 1) % 8
+           ai_boy_IQ10.handle_right_run(self)
+
+       elif self.state == self.LEFT_RUN:
+           self.dir = -1
+           #self.frame = (self.frame + 1) % 8
+           ai_boy_IQ10.handle_left_run(self)
+       elif self.state == self.LEFT_STAND:
+           #self.frame = (self.frame + 1) % 8
+           ai_boy_IQ10.handle_left_stand(self)
+       elif self.state == self.RIGHT_STAND:
+           #self.frame = (self.frame + 1) % 8
+           ai_boy_IQ10.handle_right_stand(self)
+
+
+   def draw(self):
+      self.image.clip_draw(self.frame*100, self.state * 100, 100, 100, self.x, self.y)
+
+
+
+class Grass:
+   def __init__(self):
+      self.image = load_image('grass.png')
+   def draw(self):
+      self.image.draw(400, 30)
 
 
 
@@ -91,6 +278,7 @@ def update():
 
 
 def draw():
+    global selectindex
     clear_canvas()
     grass.draw()
     boy.draw()
