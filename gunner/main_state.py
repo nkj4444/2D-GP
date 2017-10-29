6998217eda95ea76fd9819c13f2c08def3a3baf8
nkj4444 <31687469+nkj4444@users.noreply.github.com>
2017-10-30 06:01:15 +0900

    UI위치 고정, 이동클릭시 커서 변경으로 가시성 상승, 체력바 에너지바 수식 완성
    이제 적이랑 공격만 하면 되는데...

--- gunner/main_state.py
+++ gunner/main_state.py
@@ -4,17 +4,22 @@ import game_framework
 import numbers
 
 
-open_canvas()
+open_canvas(1400,1000)
 running = True
 move6_1, move8_1,move4_1,move2_1,move7_1,move9_1,move1_1,move3_1,move6_2, move8_2,move4_2,move2_2,move7_2,move9_2,move1_2,move3_2 = False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False
 move = False
+mouse_cursor_change = False
 
 gunnerX = 60
-gunnerY = 190
+gunnerY = 490
 turn = 1
+cursor_x = 300
+cursor_y = 300
+player_hp = 10
+player_ep = 10
 
 w = 20
-h = 12
+h = 15
 #왜인진 모르겠으나 범위를 넓게 잡지 않으면 리스트를 벗어난다는 오류가 뜬다
 maprect = [[to_sdl_rect(0,0,100,100) for col in range(w)] for row in range(h)]
 
@@ -29,7 +34,7 @@ player_position_matrix = [[0 for col in range(w)] for row in range(h)]
 player_position = player_position_matrix[0][1]
 """
 P_x = 0
-P_y = 1
+P_y = 4
 
 events = get_events()
 
@@ -40,6 +45,7 @@ def handle_events():
     global move
     global move6_1, move8_1,move4_1,move2_1,move7_1,move9_1,move1_1,move3_1,move6_2, move8_2,move4_2,move2_2,move7_2,move9_2,move1_2,move3_2
     global rect1
+    global cursor_x, cursor_y,mouse_cursor_change
 
     events = get_events()
     for event in events:
@@ -57,27 +63,73 @@ def handle_events():
 
 
         elif event.type == SDL_MOUSEBUTTONDOWN and turn == 1:
-            print(P_x,P_y)
 
 
-            if event.x > 50 and event.x <150 and event.y > 235 and event.y < 265:
+
+            if event.x > 300 and event.x <600 and event.y > 900 and event.y < 1000:
                 move = True
-            elif event.x > 50 and event.x < 150 and event.y > 135 and event.y < 165:
+                mouse_cursor_change = True
+
+            elif event.x > 300 and event.x < 600 and event.y > 800 and event.y < 900:
                 turn_change()
+            elif event.y < 300 or event.y > 700 and move == True:
+                print('overrange!')
+                move = False
+
         elif event.type == SDL_MOUSEBUTTONUP and turn ==1 and move == True:
+
             if event.x > maprect[P_x+1][P_y].x and event.x < maprect[P_x+2][P_y].x and event.y > maprect[P_x][P_y].y and event.y < maprect[P_x][P_y-1].y:
                 move6_1 = True
+                move8_1, move4_1, move2_1, move7_1, move9_1, move1_1, move3_1 = False,False,False,False,False,False,False
+                mouse_cursor_change = False
             elif event.x > maprect[P_x][P_y].x and event.x < maprect[P_x+1][P_y].x and event.y > maprect[P_x][P_y+1].y and event.y < maprect[P_x][P_y].y:
                 move8_1 = True
+                move6_1, move4_1, move2_1, move7_1, move9_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
             elif P_y < 2 and event.x > maprect[P_x][P_y].x and event.x < maprect[P_x+1][P_y].x and event.y > maprect[P_x][P_y-1].y and event.y < maprect[P_x][0].y+100:
                 move2_1 = True
+                move6_1, move8_1, move4_1, move7_1, move9_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
             elif P_y >= 2 and event.x > maprect[P_x][P_y].x and event.x < maprect[P_x+1][P_y].x and event.y > maprect[P_x][P_y-1].y and event.y < maprect[P_x][P_y-2].y:
                 move2_1 = True
+                move6_1, move8_1, move4_1, move7_1, move9_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
             elif P_x < 2 and event.x > maprect[0][P_y].x and event.x < maprect[1][P_y].x and event.y > maprect[P_x][P_y].y and event.y < maprect[P_x][P_y-1].y:
                 move4_1 = True
+                move6_1, move8_1, move2_1, move7_1, move9_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
             elif P_x >= 2 and event.x > maprect[P_x-1][P_y].x and event.x < maprect[P_x][P_y].x and event.y > maprect[P_x][P_y].y and event.y < maprect[P_x][P_y-1].y:
                 move4_1 = True
-
+                move6_1, move8_1, move2_1, move7_1, move9_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
+            elif event.x > maprect[P_x+1][P_y].x and event.x < maprect[P_x+2][P_y].x and event.y > maprect[P_x][P_y+1].y and event.y < maprect[P_x][P_y].y:
+                move9_1 = True
+                move6_1, move8_1, move4_1, move2_1, move7_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
+            elif event.x > maprect[P_x+1][P_y].x and event.x < maprect[P_x+2][P_y].x and event.y > maprect[P_x][P_y-1].y and event.y < maprect[P_x][P_y-2].y:
+                move3_1 = True
+                move6_1, move8_1, move4_1, move2_1, move7_1, move9_1, move1_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
+            elif P_x < 2 and event.x > maprect[0][P_y].x and event.x < maprect[1][P_y].x and event.y > maprect[P_x][P_y+1].y and event.y < maprect[P_x][P_y].y:
+                move7_1 = True
+                move6_1, move8_1, move4_1, move2_1, move9_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
+            elif P_x >= 2 and event.x > maprect[P_x-1][P_y].x and event.x < maprect[P_x][P_y].x and event.y > maprect[P_x][P_y+1].y and event.y < maprect[P_x][P_y].y:
+                move7_1 = True
+                move6_1, move8_1, move4_1, move2_1, move9_1, move1_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
+            elif P_x < 2 and event.x > maprect[0][P_y].x and event.x < maprect[1][P_y].x and event.y > maprect[P_x][P_y-1].y and event.y < maprect[P_x][P_y-2].y:
+                move1_1 = True
+                move6_1, move8_1, move4_1, move2_1, move7_1, move9_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
+            elif P_x >= 2 and event.x > maprect[P_x-1][P_y].x and event.x < maprect[P_x][P_y].x and event.y > maprect[P_x][P_y-1].y and event.y < maprect[P_x][P_y-2].y:
+                move1_1 = True
+                move6_1, move8_1, move4_1, move2_1, move7_1, move9_1, move3_1 = False, False, False, False, False, False, False
+                mouse_cursor_change = False
+        elif event.type == SDL_MOUSEMOTION:
+            if move == True and event.y < 700 and event.y > 300:
+                cursor_x = event.x
+                cursor_y = 1000 - event.y
 
 
 
@@ -94,10 +146,14 @@ def turn_change():
     global turn
     global move
 
+
+
     if turn == 1:
         turn = 2
     elif turn == 2:
         turn = 1
+        print(P_x, P_y-3)#바뀐위치
+
 
 #턴은 3 까지 해서 1 = 플레이어, 2 = 적ai, 3 = 움직임 이렇게 해야 할듯
 
@@ -112,6 +168,32 @@ class UserInterface:
         self.image.clip_draw(0, 40, 100, 40, 100, 400, 100, 30)
         self.image.clip_draw(0, 80, 100, 40, 100, 450, 100, 30)
         ##(x가시작되는위치,y가시작되는위치,표시할넓이,표시할높이,x좌표,y좌표,x축스케일,y축스케일)
+
+class Point_bar:
+    def __init__(self):
+        self.image = load_image('point_bar.png')
+    def draw_HP(self):
+        self.image.clip_draw(0, 50, 300, 50, 1015+(35/2)*player_hp, 195, 35*player_hp, 100)
+    def draw_EP(self):
+        self.image.clip_draw(0, 0, 300, 50, 1015+(35/2)*player_ep, 95, 35*player_ep, 100)
+
+
+class Mousecursor:
+    global cursor_x
+    global cursor_y
+
+
+    def __init__(self):
+        self.image = load_image('cursor.png')
+
+
+    def attack_cursor(self):
+        self.image.clip_draw(100, 0, 100, 100, cursor_x, cursor_y, 100, 100)
+
+
+    def move_cursor(self):
+        self.image.clip_draw(0, 0, 100, 100, cursor_x, cursor_y, 100, 100)
+
 
 class Gunner:
 
@@ -133,9 +215,26 @@ class Ground:
 
 
 
+
+       ##(x가시작되는위치,y가시작되는위치,표시할넓이,표시할높이,x좌표,y좌표,x축스케일,y축스케일)
+
+
+class Map:
+
+
+   def __init__(self):
+       self.image = load_image('map_test.png')
+
+   def draw(self):
+       self.image.draw(700, 500)
+
+
 gunner = Gunner()
 UI = UserInterface()
 ground = Ground()
+map = Map()
+m_cursor = Mousecursor()
+p_bar = Point_bar()
 
 while running == True:
 
@@ -156,7 +255,6 @@ while running == True:
             move = False
         elif move4_1 == True:
             gunnerX = gunnerX - 100
-
             P_x = P_x - 1
             move4_1 = False
             move = False
@@ -165,6 +263,36 @@ while running == True:
             P_y = P_y - 1
             move2_1 = False
             move = False
+        elif move7_1 == True:
+            gunnerX = gunnerX - 100
+            gunnerY = gunnerY + 100
+            P_y = P_y + 1
+            P_x = P_x - 1
+            move7_1 = False
+            move = False
+        elif move9_1 == True:
+            gunnerX = gunnerX + 100
+            gunnerY = gunnerY + 100
+            P_x = P_x + 1
+            P_y = P_y + 1
+            move9_1 = False
+            move = False
+        elif move3_1 == True:
+            gunnerX = gunnerX + 100
+            gunnerY = gunnerY - 100
+            P_x = P_x + 1
+            P_y = P_y - 1
+            move3_1 = False
+            move = False
+        elif move1_1 == True:
+            gunnerY = gunnerY - 100
+            gunnerX = gunnerX - 100
+            P_y = P_y - 1
+            P_y = P_y - 1
+            move1_1 = False
+            move = False
+
+
 
 
 
@@ -180,11 +308,16 @@ while running == True:
     elif turn ==2:
         delay(2)
         turn_change()
-    numbers.draw(turn, 400, 550, 1) # 턴 나타내는 숫자. 테스트용
-    ground.draw()
-    gunner.draw()
-    UI.draw()
 
+    #ground.draw()
+    map.draw()
+    p_bar.draw_HP()
+    p_bar.draw_EP()
+    gunner.draw()
+    #UI.draw()
+    numbers.draw(turn, 450, 250, 1)  # 턴 나타내는 숫자. 테스트용
+    if mouse_cursor_change == True:
+        m_cursor.move_cursor()
 
     update_canvas()
     clear_canvas()
